15c87fd73c052c76b6d056248d8448d6745972f8
purrck <493715706@qq.com>
2019-07-30 19:07:08 +0800

    init

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,6 +4,8 @@ import {ImprintService} from '../imprint/imprint.service';
 import {MenuService} from './menu.service';
 import {CatalogService} from '../../pages/catalog/catalog.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -33,12 +35,20 @@ export class MenuComponent implements OnInit {
     this.catalogSvc.get().subscribe(res => {
       this.catalogs = res;
     });
+
+    $(window).resize(() => {
+      $('.menu__top-part, .menu__bott-part').height(($(window).height() - $('.nav-main').height()) / 2 - 2);
+    });
   }
 
   menu(catalog, e) {
     e.preventDefault();
     this.menuSvc.set(false);
     this.router.navigate(['/catalog', catalog._id]);
+  }
+
+  hover(state, e) {
+    console.log(e);
   }
 
   toggle() {
